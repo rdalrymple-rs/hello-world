630e6eb794311915824df4bb60710c9ffeb00242
Stephan Koehler <koehler.s.a@gmail.com>
2022-01-27 17:22:42 -0500

    test again

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,8 @@
+import random
+
+def random_saying():
+    return random.sample( ['hi', 'look up', 'look down'], k = 2)
+
 if __name__ == "__main__":
     print("Hello world!")
+    print( random_saying() )
