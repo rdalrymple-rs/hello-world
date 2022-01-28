f0efef94f1919af82c3fdad8810af64a2e8324e9
Stephan Koehler <koehler.s.a@gmail.com>
2022-01-28 11:53:19 -0500

    test again

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,10 @@
 import random
 
+
 def random_saying():
-    return random.sample( ['hi', 'look up', 'look down'], k = 2)
+    return random.sample(['hi', 'look up', 'look down'], k=2)
+
 
 if __name__ == "__main__":
     print("Hello world!")
-    print( random_saying() )
+    print(random_saying())
